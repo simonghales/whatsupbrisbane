67ea724aa1a8041365a0204dc824427422283f08
James Thompson <james.thompson275@hotmail.com>
2016-07-30 13:38:35 +1000

    Added DB Schema

--- main/models.py
+++ main/models.py
@@ -3,3 +3,24 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class EventInfo(models.Model):
+	title = models.TextField()
+	description = models.TextField()
+	cost = models.TextField()
+	timeStart = models.DateTimeField()
+	timeStop = models.DateTimeField()
+	venue = models.TextField()
+	venueAddress = models.TextField()
+	location = models.TextField()
+	latitude = models.CharField()
+	longitude = models.CharField()
+	eventImage = models.TextField()
+	bookings = models.TextField()
+	category = models.TextField()
+	weblink = models.TextField()
+	age = models.TextField()
+	meetingPoint = models.TextField()
+	requirements = models.TextField()
+	showType = models.TextField()
+	schedule = models.TextField()
