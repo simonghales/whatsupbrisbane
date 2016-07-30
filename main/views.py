44f4855ecb8caeefd2ff387e96b1f93cad5e48a9
JonathanHolland <jonathan.holland8@gmail.com>
2016-07-30 14:55:27 +1000

    Properly removing reference to the Greeting db table in Views

--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
 from .models import EventInfo
 
 # Create your views here.
 def index(request):
     
+    return render(request, 'index.html')
+
+def db(request):
+
     newData = EventInfo()
     newData.title = "TestOne"
     newData.save()
@@ -14,15 +17,4 @@ def index(request):
     dbData = EventInfo.objects.all()
     
     return render(request, 'db.html', {'greetings': dbdata})
-    #return render(request, 'index.html')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
 
