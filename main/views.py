599af720d5bf5667e247df22b3f0b83919879190
JonathanHolland <jonathan.holland8@gmail.com>
2016-07-30 14:03:45 +1000

    Test code for Thompson's new database table
    - It now replaces the previous example static index.html page

--- main/views.py
+++ main/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import EventInfo
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    
+    newData = EventInfo()
+    newData.title = "TestOne"
+    newData.save()
+
+    dbData = EventInfo.objects.all()
+    
+    return render(request, 'db.html', {'greetings': dbdata})
+    #return render(request, 'index.html')
 
 
 def db(request):
