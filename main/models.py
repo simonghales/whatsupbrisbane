038a9cb6eed958ddf7a12b5cdb889977cd96fdaa
JonathanHolland <jonathan.holland8@gmail.com>
2016-07-30 19:41:56 +1000

    Adding addressLocation table, removing location and adding imageURL to EventInfo table
    - also adding defaults for the textFields

--- main/models.py
+++ main/models.py
@@ -1,23 +1,34 @@
 from django.db import models
 
+def text_default():
+        return ""
+
 class EventInfo(models.Model):
-	title = models.TextField()
-	description = models.TextField()
-	cost = models.TextField()
+        
+	title = models.TextField(default=text_default)
+	description = models.TextField(default=text_default)
+	cost = models.TextField(default=text_default)
 	timeStart = models.DateTimeField(null=True, blank=True)
 	timeStop = models.DateTimeField(null=True, blank=True)
-	venue = models.TextField()
-	venueAddress = models.TextField()
-	location = models.TextField()
-	latitude = models.DecimalField(max_digits=10,decimal_places=7, null=True, blank=True)
-	longitude = models.DecimalField(max_digits=10,decimal_places=7, null=True, blank=True)
-	eventImage = models.TextField()
-	bookings = models.TextField()
-	category = models.TextField()
-	weblink = models.TextField()
-	age = models.TextField()
-	meetingPoint = models.TextField()
-	requirements = models.TextField()
-	showType = models.TextField()
-	schedule = models.TextField()
+	venue = models.TextField(default=text_default)
+	venueAddress = models.TextField(default=text_default)
+	latitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+	longitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+	eventImage = models.TextField(default=text_default)
+	bookings = models.TextField(default=text_default)
+	category = models.TextField(default=text_default)
+	weblink = models.TextField(default=text_default)
+	age = models.TextField(default=text_default)
+	meetingPoint = models.TextField(default=text_default)
+	requirements = models.TextField(default=text_default)
+	showType = models.TextField(default=text_default)
+	schedule = models.TextField(default=text_default)
 	outdoors = models.NullBooleanField()
+	imageURL = models.TextField(default=text_default)
+
+class AddressLocation(models.Model):
+
+        address = models.TextField(default=text_default)
+        latitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+        longitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+        
